eeeeeb15873a18d2947f9a93feecd1319e2ca570
elbahi03 <yassineelbahi44@gmail.com>
2025-07-24 13:57:51 +0200

    js

---
+++ src/reservation/reservation.js
@@ -0,0 +1,26 @@
+document.getElementById('reserve').onclick = function (event) {
+
+    event.preventDefault();
+
+    const form = document.reservation;
+    const email = form.email.value;
+    const phone = form.numero.value;
+
+    if (!form.nom.value || !email || !phone || !form.date.value) {
+        alert("Veuillez remplir tous les champs !");
+        return false;
+    }
+
+    if (!email.includes('@') || !email.includes('.')) {
+        alert("Adresse e-mail incorrecte !");
+        return false;
+    }
+
+    if (phone.length !== 10 || isNaN(phone)) {
+        alert("Le numéro doit contenir 10 chiffres !");
+        return false;
+    }
+
+    alert('Formulaire validé ! Vous recevrez un appel de confirmation.');
+    return true;
+}
